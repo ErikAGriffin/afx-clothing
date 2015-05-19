c94522ea5991091b9ad1954af8a35942f3b57fba
Erik Griffin <griffin.erik@gmail.com>
2015-05-19 11:27:44 +0200

    Removed strange character in some product titles'

--- public/js/app.js
+++ public/js/app.js
@@ -9,7 +9,7 @@
 
     $http.post('/products').success(function(data,status) {
       self.products = data;
-      console.log(self.products[0]);
+      console.log(self.products[9]);
     })
     .error(function(){
       console.log('error fetching products');
