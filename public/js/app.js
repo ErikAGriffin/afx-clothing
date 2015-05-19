0de097986aee71b913845735cf60d3e28c5f9487
Erik Griffin <griffin.erik@gmail.com>
2015-05-19 21:08:49 +0200

    Added filtering system

--- public/js/app.js
+++ public/js/app.js
@@ -15,20 +15,55 @@
       product.activeColor = color;
     };
 
-    self.resetFilter = function() {
+    self.showMenOptions = false;
+    self.showWomenOptions = false;
 
+    self.hoverMen = function() {
+      self.showWomenOptions = false;
+      self.showMenOptions = true;
+    };
+
+    self.hoverWomen = function() {
+      self.showMenOptions = false;
+      self.showWomenOptions = true;
+    };
+
+    self.hoverClear = function() {
+      self.showMenOptions = false;
+      self.showWomenOptions = false;
+    };
+
+    self.productFilter = {
+      gender: false,
+      category: false
+    };
+
+    self.resetFilter = function() {
+      self.productFilter.gender = false;
+      self.productFilter.category = false;
     };
 
     self.filterByMen = function(filter) {
-
+      self.productFilter.gender = 'Mens';
+      if (!filter) {self.productFilter.category = false;}
+      else {self.productFilter.category = filter;}
     };
 
     self.filterByWomen = function(filter) {
-
+      self.productFilter.gender = 'Womens';
+      if (!filter) {self.productFilter.category = false;}
+      else {self.productFilter.category = filter;}
     };
 
     self.isProductShown = function(product) {
-      return true;
+      if (!self.productFilter.gender) {return true;}
+      else if (!self.productFilter.category) {
+        return product.gender === self.productFilter.gender;
+      }
+      else {
+        return (product.gender === self.productFilter.gender) &&
+          (product.category === self.productFilter.category);
+      }
     };
 
     var setActiveColor = function(productsArray) {
