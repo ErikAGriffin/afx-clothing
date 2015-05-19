071487fbc30ca14c482bb3d54c895b3158ae0e63
Erik Griffin <griffin.erik@gmail.com>
2015-05-19 10:41:08 +0200

    Added mock images

--- public/js/app.js
+++ public/js/app.js
@@ -9,7 +9,7 @@
 
     $http.post('/products').success(function(data,status) {
       self.products = data;
-      console.log(self.products);
+      console.log(self.products[0]);
     })
     .error(function(){
       console.log('error fetching products');
