687ba10c22158dd94d23fb15ca347b9e84a777a0
Erik Griffin <griffin.erik@gmail.com>
2015-05-20 11:52:40 +0200

    Added filter tests

--- spec/features/firstVist.spec.js
+++ spec/features/firstVist.spec.js
@@ -4,6 +4,15 @@ describe('First Visit to AFX', function() {
 
   var titles = element.all(by.binding('product.name'));
   var mens = element(by.cssContainingText('a', 'Men'));
+  var womens = element(by.cssContainingText('a','Women'));
+  var mensFormal = $('a[ng-click="productsCtrl.filterByMen(\'Formalwear\')"]');
+  var womensFootwear = $('a[ng-click="productsCtrl.filterByWomen(\'Footwear\')"]');
+
+  var hoverOver = function(element) {
+    browser.actions().
+    mouseMove(element).
+    perform();
+  };
 
   beforeEach(function() {
     browser.get('http://localhost:3000');
@@ -17,6 +26,9 @@ describe('First Visit to AFX', function() {
 
   });
 
+  // Consequentially, this test also depends on the order
+  // of the products.json. Hm.
+
   it('Begins with available products listed', function() {
     expect(titles.get(0).getText()).toEqual('Almond Toe Court Shoes');
     expect(titles.get(10).getText()).toEqual('Mid Twist Cut-Out Dress');
@@ -24,7 +36,7 @@ describe('First Visit to AFX', function() {
 
   // This test is dependent on the order of products not changing.
   // I'm not sure how to check a random or arbitrary male and
-  // female items, without adding additional hmtl
+  // female items, without adding unnecessary html to the page
 
   it('Will filter products by gender', function() {
     expect(titles.get(0).isDisplayed()).toBeTruthy();
@@ -32,6 +44,25 @@ describe('First Visit to AFX', function() {
     mens.click();
     expect(titles.get(0).isDisplayed()).toBeFalsy();
     expect(titles.get(2).isDisplayed()).toBeTruthy();
+    womens.click();
+    expect(titles.get(0).isDisplayed()).toBeTruthy();
+    expect(titles.get(2).isDisplayed()).toBeFalsy();
+  });
+
+  // As a matter of fact, all of my tests are dependent
+  // on the ordering of products in products.json,
+  // or in a real application the order which they are
+  // retrieved from the database.
+
+  it('Will filter products by gender+category', function() {
+    hoverOver(mens);
+    mensFormal.click();
+    expect(titles.get(9).isDisplayed()).toBeFalsy();
+    expect(titles.get(7).isDisplayed()).toBeTruthy();
+    hoverOver(womens);
+    womensFootwear.click();
+    expect(titles.get(0).isDisplayed()).toBeTruthy();
+    expect(titles.get(2).isDisplayed()).toBeFalsy();
   });
 
 
